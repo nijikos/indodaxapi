10a06e637124c669863cf351532c6543479cb6dc
nijiko <nijikos@gmail.com>
2021-02-01 21:56:59 +0700

    added react reveal Fade bottom

--- src/component/Container.js
+++ src/component/Container.js
@@ -6,6 +6,7 @@ import Watermark from "./Watermark";
 import WrapperTable from "./WrapperTable";
 import WrapperTicker from "./WrapperTicker";
 import WrapperVolBtc from "./WrapperVolBtc";
+import Fade from "react-reveal/Fade";
 
 class Container extends Component {
   render() {
@@ -14,10 +15,13 @@ class Container extends Component {
         <div className="container-child">
           <Watermark />
           <Title />
-          <WrapperVolBtc />
-          <WrapperTicker />
-          <WrapperTable />
-          <Credits />
+          <Fade bottom>
+            <WrapperVolBtc />
+            <WrapperTicker />
+            <WrapperTable />
+            <Credits />
+          </Fade>
+
           {/* ========================= */}
         </div>
       </div>
